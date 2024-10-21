474c8109765ea5f38a7a459f06cd16cfb3d5ff2f
ivaquero <msaintms@outlook.com>
2024-10-21 14:40:16 +0800

    update brew

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,10 +1,2 @@
-atreplinit() do repl
-    try
-        @eval using OhMyREPL
-    catch e
-        @warn "error while importing OhMyREPL" e
-    end
-end
-
 ENV["JULIA_NUM_THREADS"] = 8
-ENV["JULIA_PKG_SERVER"] = "https://mirrors.pku.edu.cn/julia/"
+# ENV["JULIA_PKG_SERVER"] = "https://mirrors.ustc.edu.cn/julia"
