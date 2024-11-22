be5666299f81cfdc9a1406e01b5315d8ac5bc7a8
ivaquero <msaintms@outlook.com>
2024-11-22 23:01:40 +0800

    update scoop

--- shell/Profile.ps1
+++ shell/Profile.ps1
@@ -6,7 +6,7 @@ Import-Module scoop-completion
 # #region mamba initialize
 # # !! Contents within this block are managed by 'mamba shell init' !!
 # $Env:MAMBA_ROOT_PREFIX = "$HOME\micromamba"
-# $Env:MAMBA_EXE = "C:\Scoop\apps\micromamba-cn\current\Library\bin\micromamba.exe"
+# $Env:MAMBA_EXE = "C:\Scoop\apps\micromamba\current\micromamba.exe"
 # (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 # #endregion
 
