22e66cf6c97686400e5f85412bf513a97d2c8cfc
ivaquero <msaintms@outlook.com>
2025-02-15 15:03:49 +0800

    chore: 🚸 update vscode

--- shell/Profile.ps1
+++ shell/Profile.ps1
@@ -4,15 +4,8 @@
 Import-Module scoop-completion
 
 # #region mamba initialize
-# # !! Contents within this block are managed by 'mamba shell init' !!
-# $Env:MAMBA_ROOT_PREFIX = "$HOME\micromamba"
-# $Env:MAMBA_EXE = "C:\Scoop\apps\micromamba\current\micromamba.exe"
-# (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+# !! Contents within this block are managed by 'mamba shell init' !!
+$Env:MAMBA_ROOT_PREFIX = "$HOME\micromamba"
+$Env:MAMBA_EXE = "C:\Scoop\apps\micromamba\shims\micromamba.exe"
+(& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 # #endregion
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Scoop\apps\miniforge-cn\current\Scripts\conda.exe") {
-    (& "C:\Scoop\apps\miniforge-cn\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
-}
-#endregion
